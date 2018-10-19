875437b7181df1458496debdf6c5bff8983b7af0
Brent Kirkland <brentckirkland@gmail.com>
2018-10-19 11:37:19 -0700

    update webhook

--- webhook_server.js
+++ webhook_server.js
@@ -13,14 +13,22 @@ const port = 3000
 app.use(bodyParser.json()) // must use bodyParser in express
 app.use(webhookHandler) // use our middleware
 
-webhookHandler.on('event', function (repo, data) {
-  console.log('repo', repo)
-});
-
-webhookHandler.on('reponame', function (event, data) {
+webhookHandler.on('pull_request', function (repo, data) {
+  const { action, pull_request } = data
+  const { merged, title, body } = pull_request
+  if (action === 'closed' && merged) {
+    console.log('repo: ', repo)
+    console.log('title', title)
+    console.log('body', body)
+  }
 });
 
 webhookHandler.on('error', function (err, req, res) {
+  console.log({
+    err,
+    req,
+    res
+  })
 });
 
 app.listen(port, () => console.log(`Webhook server listening on port ${port}!`))
